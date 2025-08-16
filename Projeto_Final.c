ba4373a3b7084b043cd4e5b6f0ea0f1cfbe404e5
Thiagogomes1805 <thiago.oliveira.712@ufrn.edu.br>
2025-08-16 18:28:11 -0300

    Feat Projeto_Final.c
    Adding employers module

---
+++ Projeto_Final.c
@@ -0,0 +1,84 @@
+void TelaMenuFuncionarios() {
+    system("cls");
+    printf("----------------------------------------\n");
+    printf("///           Módulo Funcionários         ///\n");
+    printf("----------------------------------------\n");
+    printf("///                                     ///\n");
+    printf("///     1. Cadastrar Funcionários       ///\n");
+    printf("///     2. Pesquisar Funcionários       ///\n");
+    printf("///     3. Alterar Funcionários         ///\n");
+    printf("///     4. Excluir Funcionários         ///\n");
+    printf("///     0. Voltar ao Menu Principal     ///\n");
+    printf("///                                     ///\n");
+    printf("----------------------------------------\n");
+    printf(">>> Escolha a opção desejada: ");
+}
+
+void TelaCadastrarFuncionarios() {
+    system("cls");
+    printf("----------------------------------------\n");
+    printf("///       Cadastrar Funcionário      ///\n");
+    printf("----------------------------------------\n");
+    printf("///                                  ///\n");
+    printf("///   CPF:                           ///\n");
+    printf("///   Nome:                          ///\n");
+    printf("///   Telefone:                      ///\n");
+    printf("///   E-mail:                        ///\n");
+    printf("///                                  ///\n");
+    printf("----------------------------------------\n");
+    printf("\n");
+}
+
+void TelaPesquisarFuncionarios() {
+    system("cls");
+    printf("----------------------------------------\n");
+    printf("///       Pesquisar Funcionário      ///\n");
+    printf("----------------------------------------\n");
+    printf("///                                  ///\n");
+    printf("///   Informe o CPF do funcionário:   ///\n");
+    printf("///                                  ///\n");
+    printf("----------------------------------------\n");
+    printf("\n");
+}
+
+void TelaAlterarFuncionarios() {
+    system("cls");
+    printf("----------------------------------------\n");
+    printf("///        Alterar Funcionário       ///\n");
+    printf("----------------------------------------\n");
+    printf("///                                  ///\n");
+    printf("///   Informe o CPF do funcionário:   ///\n");
+    printf("///                                  ///\n");
+    printf("----------------------------------------\n");
+    printf("\n");
+}
+
+void TelaExcluirFuncionarios() {
+    system("cls");
+    printf("----------------------------------------\n");
+    printf("///        Excluir Funcionário       ///\n");
+    printf("----------------------------------------\n");
+    printf("///                                  ///\n");
+    printf("///   Informe o CPF do funcionário:   ///\n");
+    printf("///                                  ///\n");
+    printf("----------------------------------------\n");
+    printf("\n");
+}
+
+void TelaConfirmarExclusao() {
+    system("cls");
+    printf("----------------------------------------\n");
+    printf("///      Confirmar Exclusão?         ///\n");
+    printf("----------------------------------------\n");
+    printf("///                                  ///\n");
+    printf("///   1. Sim (S/s)                   ///\n");
+    printf("///   2. Não (N/n)                   ///\n");
+    printf("///                                  ///\n");
+    printf("----------------------------------------\n");
+    printf(">>> Escolha a opção desejada: ");
+}
+
+void limpar_tela(void) {
+    system("cls");
+}
+
